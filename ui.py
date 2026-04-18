5bac2d1c070510759447e47adf68c71afe520142
Vikrant Kumar <khaiwal.vikrant@gmail.com>
2026-04-18 23:18:36 +0530

    Update ui.py
    new line added

--- ui.py
+++ ui.py
@@ -125,3 +125,4 @@ with gr.Blocks(title="IVF Treatment Advisor") as demo:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
     demo.launch(server_name="0.0.0.0", server_port=port)
+
